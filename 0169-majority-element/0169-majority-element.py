9c567b4870578fa374772398ad257d3b393b31dc
Astha Gulhane <asthagulhane04@gmail.com>
2026-07-29 23:31:34 +0530

    Time: 7 ms (57.31%), Space: 21 MB (84.82%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,48 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         majority_num = None 
+#         count = 0
+
+#         for num in  nums :
+#             if count == 0  :
+#                 majority_num = num 
+
+#             if  num == majority_num :
+#                 count += 1
+#             else:
+#                 count -= 1
+#         return majority_num
+
+
+
+
+# class Solution:
+#     def majorityElement(self, nums):
+
+#         freq = {}
+
+#         for num in nums:
+
+#             freq[num] = freq.get(num, 0) + 1
+
+#             if freq[num] > len(nums) // 2:
+#                 return num
+
+
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        majority_num = None 
+    def majorityElement(self, nums):
+
+        candidate = None
         count = 0
 
-        for num in  nums :
-            if count == 0  :
-                majority_num = num 
+        for num in nums:
 
-            if  num == majority_num :
+            if count == 0:
+                candidate = num
+
+            if num == candidate:
                 count += 1
             else:
                 count -= 1
-        return majority_num
+
+        return candidate
