e5cb657cfabc17fa225c01a4c9b381c3d3a4799d
Astha Gulhane <asthagulhane04@gmail.com>
2026-07-30 19:58:34 +0530

    Time: 7 ms (37.61%), Space: 19.7 MB (77.87%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,13 +1,41 @@
+# class Solution:
+#     def maxProduct(self, nums: List[int]) -> int:
+#         max_product = nums[0]
+#         n = len(nums) 
+
+#         for left in range(n):
+#             current_product = 1 
+#             for right  in range (left , n ):
+#                 current_product *= nums[right] 
+
+#                 if current_product > max_product :
+#                     max_product =current_product
+#         return max_product
+
+
+
+
+
+
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        max_product = nums[0]
-        n = len(nums) 
+        max_ending = nums[0]
+        min_ending = nums[0]
+        answer = nums[0]
 
-        for left in range(n):
-            current_product = 1 
-            for right  in range (left , n ):
-                current_product *= nums[right] 
+        for num in nums[1:]:
+            current_max = max(num,
+                              num * max_ending,
+                              num * min_ending)
 
-                if current_product > max_product :
-                    max_product =current_product
-        return max_product
+            current_min = min(num,
+                              num * max_ending,
+                              num * min_ending)
+
+            max_ending = current_max
+            min_ending = current_min
+
+            answer = max(answer, max_ending)
+
+        return answer
