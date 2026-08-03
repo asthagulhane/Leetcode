528d74bd89d20979876b87137447504f0a3ea2b0
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-03 22:34:00 +0530

    Time: 0 ms (100%), Space: 19.3 MB (25.7%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.py
@@ -0,0 +1,27 @@
+   
+
+class Solution:
+    def nextPermutation(self, nums: List[int]) -> None:
+        n = len(nums)
+
+        # Step 1: Find pivot
+        pivot = n - 2
+        while pivot >= 0 and nums[pivot] >= nums[pivot + 1]:
+            pivot -= 1
+
+        # Step 2: Find next greater element
+        if pivot >= 0:
+            j = n - 1
+            while nums[j] <= nums[pivot]:
+                j -= 1
+
+            nums[pivot], nums[j] = nums[j], nums[pivot]
+
+        # Step 3: Reverse suffix
+        left = pivot + 1
+        right = n - 1
+
+        while left < right:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+            right -= 1
