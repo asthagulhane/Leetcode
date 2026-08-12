7116226ece38308f2c2c5d37a6e15ed0b72424c5
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-12 16:42:32 +0530

    Time: 7 ms (39.16%), Space: 20.5 MB (62.04%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,19 +1,30 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        # create a temporary array 
-        temp = []
+# class Solution:
+#     def moveZeroes(self, nums: List[int]) -> None:
+#         # create a temporary array 
+#         temp = []
 
-        # Step 1 :collect all non - zero elements
-        for num in nums:
-            if  num != 0 :
-                temp.append(num)
+#         # Step 1 :collect all non - zero elements
+#         for num in nums:
+#             if  num != 0 :
+#                 temp.append(num)
 
-        # Step 2 : Fill the remaining place with order
-        while len(temp) < len(nums):
-            temp.append(0)
+#         # Step 2 : Fill the remaining place with order
+#         while len(temp) < len(nums):
+#             temp.append(0)
         
-        # Step 3: Copy the elements back into the original array
-        for i in range(len(nums)):
-            nums[i] = temp[i]
+#         # Step 3: Copy the elements back into the original array
+#         for i in range(len(nums)):
+#             nums[i] = temp[i]
 
        
+
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        insert_Pos = 0 
+
+        for current  in  range (len(nums)):
+            if nums[current] != 0 :
+                nums[insert_Pos], nums[current] = nums[current], nums[insert_Pos]
+                insert_Pos += 1
