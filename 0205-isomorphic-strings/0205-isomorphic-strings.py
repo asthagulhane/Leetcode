b13ba3b60f048989c7ef51334ab00d117692af7a
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-13 22:47:14 +0530

    Time: 3 ms (91.3%), Space: 19.2 MB (82.53%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) !=  len(t) :
+            return False
+
+        for i in range (len(s)):
+            if s.find (s[i]) != t.find (t[i]) :
+                return False
+
+        return True      
