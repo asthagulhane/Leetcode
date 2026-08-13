2369730a945797dafafb7eb31f1c3bc53c5596f8
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-13 23:40:24 +0530

    Time: 0 ms (100%), Space: 23.4 MB (45.02%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -1,8 +1,20 @@
+# class Solution:
+#     def reverseString(self, s: List[str]) -> None:
+#         result = []
+        
+#         for i in  range (len(s) - 1 , -1 , -1):
+#             result.append(s[i])
+#         for i in range(len(s)):
+#             s[i] = result[i]
+
+
+
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        result = []
-        
-        for i in  range (len(s) - 1 , -1 , -1):
-            result.append(s[i])
-        for i in range(len(s)):
-            s[i] = result[i]
+        left = 0 
+        right = len(s) - 1
+
+        while left < right :
+            s[left], s[right] = s[right],s[left]
+            left +=  1 
+            right -= 1
