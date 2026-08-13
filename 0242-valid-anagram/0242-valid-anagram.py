94d40111104e43fcb41d62e08c99f6c824630984
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-13 20:49:04 +0530

    Time: 15 ms (44.44%), Space: 19.4 MB (75.77%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,7 +1,29 @@
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#             return False
+
+#         return sorted(s) == sorted(t)
+        
+
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
-            return False
+         return False
 
-        return sorted(s) == sorted(t)
-        
+        freq = {}
+
+        for ch in s:
+            freq[ch] = freq.get(ch,0) + 1
+
+        for ch  in  t:
+            if ch not in freq:
+                return False
+
+            freq[ch] -= 1
+
+            if freq[ch] < 0 :
+                return False 
+        return True
