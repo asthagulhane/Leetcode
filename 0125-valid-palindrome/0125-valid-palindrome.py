06a45d196a2ee092f9352048f057f5b6e67e339f
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-13 23:28:31 +0530

    Time: 11 ms (31.36%), Space: 19.5 MB (76.76%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,34 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         cleaned = ""
+#         for ch in s :
+#             if ch.isalnum() :
+#                 cleaned += ch.lower()
+#         return cleaned == cleaned [:: -1]
+
+
+
+
+
+
+
+
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        cleaned = ""
-        for ch in s :
-            if ch.isalnum() :
-                cleaned += ch.lower()
-        return cleaned == cleaned [:: -1]
+       left = 0 
+       right = len (s)  - 1
+       while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+
+            if s[left].lower() != s[right].lower():
+                return False  # Not a palindrome
+            
+            left += 1   # Move left pointer inward
+            right -= 1  # Move right pointer inward
+
+       return True  #
+   
