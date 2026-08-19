0da46f61170a7b28ceb3e90126a5e1650ef9a93d
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-19 18:38:46 +0530

    Time: 15 ms (44.21%), Space: 20.3 MB (10.15%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,22 +8,68 @@
 
 
 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         if len(s) != len(t):
+#          return False
+
+#         freq = {}
+
+#         for ch in s:
+#             freq[ch] = freq.get(ch,0) + 1
+
+#         for ch  in  t:
+#             if ch not in freq:
+#                 return False
+
+#             freq[ch] -= 1
+
+#             if freq[ch] < 0 :
+#                 return False 
+#         return True
+
+
+
+
+
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+       
+        return sorted(s) == sorted(t) 
+        
+
+        return counter(s) == counter(t)
         if len(s) != len(t):
-         return False
+            return False 
+            
+            countS, countT = {} , {}
+            
+            for  i in range(len(s)):
+                counts[s[i]] = 1 + counts.get(s[i], 0)
+                counts[t[i]] = 1 + counts.get(t[i], 0)
+                for c in counts:
+                    if countS[c] != countT.get(c,0):
+                        return False 
 
-        freq = {}
+                return True 
 
-        for ch in s:
-            freq[ch] = freq.get(ch,0) + 1
 
-        for ch  in  t:
-            if ch not in freq:
-                return False
 
-            freq[ch] -= 1
 
-            if freq[ch] < 0 :
-                return False 
-        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
