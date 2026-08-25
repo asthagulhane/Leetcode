7032c78e7d02488bc0263e2a2087a00b976aeedd
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-25 13:04:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (94.6%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,14 +1,38 @@
+# class Solution:
+#     def generateParenthesis(self, n: int) -> list[str]:
+#         def dfs(left, right, s=""):
+#             if left == right == 0:
+#                 return [s]
+            
+#             res = []
+#             if left > 0:
+#                 res += dfs(left - 1, right, s + "(")
+#             if right > left:
+#                 res += dfs(left, right - 1, s + ")")
+#             return res
+            
+#         return dfs(n, n)
+
+
+
+
 class Solution:
     def generateParenthesis(self, n: int) -> list[str]:
-        def dfs(left, right, s=""):
-            if left == right == 0:
-                return [s]
+        res = []
+        
+        def backtrack(open_count, close_count, current_str):
+            # Base case: valid combination found
+            if open_count == close_count == n:
+                res.append(current_str)
+                return
             
-            res = []
-            if left > 0:
-                res += dfs(left - 1, right, s + "(")
-            if right > left:
-                res += dfs(left, right - 1, s + ")")
-            return res
-            
-        return dfs(n, n)
+            # Add an open parenthesis if we haven't reached the limit
+            if open_count < n:
+                backtrack(open_count + 1, close_count, current_str + "(")
+                
+            # Add a close parenthesis if it won't violate well-formed rules
+            if close_count < open_count:
+                backtrack(open_count, close_count + 1, current_str + ")")
+                
+        backtrack(0, 0, "")
+        return res
