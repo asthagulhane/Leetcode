ede32d139cb8630de03c04312a68ce461261d09f
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-28 16:59:38 +0530

    Time: 0 ms (100%), Space: 19.9 MB (47.96%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.py
+++ 0867-transpose-matrix/0867-transpose-matrix.py
@@ -1,8 +1,16 @@
+# class Solution:
+
+#     def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
+#         # Transpose using standard nested loop comprehension swapping row/col indices
+#         return [
+#             [matrix[r][c] for r in range(len(matrix))]
+#             for c in range(len(matrix[0]))
+#         ]
+
+
+
+
 class Solution:
 
     def transpose(self, matrix: list[list[int]]) -> list[list[int]]:
-        # Transpose using standard nested loop comprehension swapping row/col indices
-        return [
-            [matrix[r][c] for r in range(len(matrix))]
-            for c in range(len(matrix[0]))
-        ]
+        return [list(col) for col in zip(*matrix)]
