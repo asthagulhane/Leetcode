8ecb1304d52b6a807ef18ebdb9c8e95b538b67b8
Astha Gulhane <asthagulhane04@gmail.com>
2026-08-29 14:15:23 +0530

    Time: 42 ms (79.58%), Space: 19.2 MB (70.42%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,13 +1,7 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        if x< 0:
-            val = - int(str(abs (x)) [::-1])
-        else :
-            val = int(str(x) [::-1])
-        if -2**31 <= val <= 2**31 - 1:
-            return val 
-        return 0
-
-    
-       
-          
+        # Reverse the absolute value as a string, then restore the sign
+        res = int(str(abs(x))[::-1]) * (-1 if x < 0 else 1)
+        
+        # Check for 32-bit signed integer overflow bounds
+        return res if -(2**31) <= res <= 2**31 - 1 else 0
